d986230b87ab5cdf66ac82708f7f29f1f212e522
yelloy <umedamir@gmail.com>
2019-07-04 12:19:41 +0300

    minor fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ module1 = Extension("openhmd",
                     ["pyopenhmd.pyx", "OpenHMD.cpp"],
                     language="c++",
                     libraries=["openhmd"],
-                    include_dirs=['/usr/include/openhmd', 'usr/local/include/openhmd'])
+                    include_dirs=['/usr/include/openhmd', '/usr/local/include/openhmd'])
 
 setup(name='openhmd',
       version='1.0',
